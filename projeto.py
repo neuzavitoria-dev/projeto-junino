0ee87e3d4714cc3da40e4e4d53e8fb452a9f52bc
Neuza Vitoria <neuzav028@gmail.com>
2026-06-26 11:08:21 -0300

    mostrar resultado e nivel

--- projeto.py
+++ projeto.py
@@ -24,6 +24,20 @@ if resposta == perguntas["resposta"]:
 else:
     print("errou")
 
+print("\nFim do jogo!")
+print(f"Sua pontuação foi: {pontos}")
+
+if pontos >= 90:
+    print("Mestre Junino!")
+
+elif pontos >= 70:
+    print("Arretado!")
+
+elif pontos >= 50:
+    print("Dançarino!")
+
+else:
+    print("Continue treinando!")
 
 
     
