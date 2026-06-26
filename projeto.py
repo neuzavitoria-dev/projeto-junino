b7ac1ae975bbb27b699b3544357ef1d08596d665
Marcos André <marcosandre.viana@gmail.com>
2026-06-26 09:26:33 -0300

    for para perguntas

--- projeto.py
+++ projeto.py
@@ -3,6 +3,12 @@ from perguntas import perguntas
 
 perguntas_sorteadas = random.sample(perguntas, 10)
 
+pontos = 0
+
+for perguntas in perguntas_sorteadas:
+    print("\n" + pergunta["pergunta"])
+    
+
 print("Sejam bem-vindos ao Arraiá do cumpadi python! ")
 
 print("Vamos testar o seu conhecimento junino, sô! ")
